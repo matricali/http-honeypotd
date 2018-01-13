03cf7f3d70774d8439cdf85760fcf45c3f1a876b
Jorge Matricali <jorgematricali@gmail.com>
2018-01-13 19:07:57 -0300

    + Handle requests (first draft)

--- http-honeypotd.c
+++ http-honeypotd.c
@@ -4,12 +4,62 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-// #include <fcntl.h>
+#include <string.h>
 
+#define BUFSIZE 8096
+#define SERVER_HEADER "jorgee"
 
 void usage(char *ex)
 {
     printf("usage: %s [-p PORT]\n", ex);
+}
+
+void process_request(int socket_fd)
+{
+	long i;
+    long ret;
+	static char buffer[BUFSIZE + 1];
+
+	ret = read(socket_fd, buffer, BUFSIZE);
+
+	if (ret == 0 || ret == -1) {
+        (void) write(socket_fd, "HTTP/1.1 403 Forbidden\nContent-Length: 185\nConnection: close\nContent-Type: text/html\n\n<html><head>\n<title>403 Forbidden</title>\n</head><body>\n<h1>Forbidden</h1>\nThe requested URL, file type or operation is not allowed on this simple static file webserver.\n</body></html>\n", 271);
+        (void) close(socket_fd);
+
+        fprintf(stderr, "%s\tHTTP/1.1 403 Forbidden\tFailed to read browser request.\n", "asdsd");
+	}
+
+	if (ret > 0 && ret < BUFSIZE) {
+        /* Cerrar buffer */
+		buffer[ret] = 0;
+    } else {
+        buffer[0] = 0;
+    }
+
+	for (i = 0; i < ret; i++) {
+        /* Eliminar CR LF */
+		if (buffer[i] == '\r' || buffer[i] == '\n') {
+			buffer[i] = '*';
+        }
+    }
+
+    printf("REQUEST >> %s\n", buffer);
+
+    char *body = "puto el que lee\n";
+
+    (void) sprintf(
+        buffer,
+        "HTTP/1.1 200 OK\nServer: %s\nContent-Length: %ld\nConnection: close\nContent-Type: text/plain\n\n%s",
+        SERVER_HEADER,
+        strlen(body),
+        body
+    );
+
+	(void) write(socket_fd, buffer, strlen(buffer));
+
+	sleep(1);
+	close(socket_fd);
+	exit(1);
 }
 
 int main(int argc, char **argv)
@@ -63,7 +113,7 @@ int main(int argc, char **argv)
     for (;;) {
 		len = sizeof(cli_addr);
 
-        socketfd = accept(listenfd, (struct sockaddr *)&cli_addr, &len);
+        socketfd = accept(listenfd, (struct sockaddr *) &cli_addr, &len);
 
         char client_address[INET_ADDRSTRLEN];
         inet_ntop(AF_INET, &(cli_addr.sin_addr), client_address, INET_ADDRSTRLEN);
@@ -71,10 +121,10 @@ int main(int argc, char **argv)
 		if (socketfd < 0) {
             fprintf(stderr, "Cannot accept incoming connection from %s\n", client_address);
             (void) close(socketfd);
-            continue;
+            return EXIT_FAILURE;
         }
 
-        printf("Incoming connection from %s", client_address);
+        printf("Incoming connection from %s\n", client_address);
 
         pid = fork();
 		if (pid < 0) {
@@ -84,7 +134,7 @@ int main(int argc, char **argv)
 
         if (pid == 0) {
 			(void) close(listenfd);
-			/* Aca tendriamos que manejar la peticion */
+			process_request(socketfd);
 		} else {
 			(void) close(socketfd);
 		}
